e82d3dbc69151489096887f4214dc4f7ac1671c7
theonekingboo <Juankingboo@proton.me>
2024-02-08 16:48:42 -0500

    added buttons and stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import Buttons from "./components/Buttons";
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-center">
-      Kookoo!
-    </h1>
-  )
+    <div className="App">
+      <h1 className="text-3xl font-bold underline text-center">Kookoo!</h1>
+      <Buttons />
+    </div>
+  );
 }
